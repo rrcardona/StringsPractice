e2795dcd54b96f2d3c6dc31128a7a3dfece44407
rrcardona <cardonas05@yahoo.com>
2017-08-22 20:50:11 -0400

    Worked on practice problems

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -10,6 +10,75 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            //string firstName = "Ricardo";
+            //string lastName = "Cardona";
+            //string fullName = firstName + ""+lastName;
+            //or
+            //string fullName = string.Concat(firstName," ", lastName);
+            //Console.WriteLine(firstName.Length);
+            //Console.WriteLine(lastName.Length);
+            //Console.WriteLine(firstName[0]);
+            //Console.WriteLine(firstName[firstName.Length-1]);
+            //Console.WriteLine(fullName);
+            //Console.WriteLine(firstName.Equals(lastName));
+            //Console.WriteLine(firstName == lastName);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine("My favorite book is the \"NoteBook\"");
+
+            //indexof and lastindexof examples
+            //Console.WriteLine(fullName.IndexOf("i"));
+            //Console.WriteLine(fullName.LastIndexOf("r"));
+
+           
+            
+            //Console.WriteLine("What is your first name");
+            //string firstName = (Console.ReadLine());
+            //Console.WriteLine("What is your last name");
+            //string lastName = (Console.ReadLine());
+            //if (firstName.Length == lastName.Length)
+            //{
+            //    Console.WriteLine("Samsis");
+            //}
+            //else if (firstName.Length > lastName.Length)
+            //{
+            //    Console.WriteLine("First is longer");
+            //}
+            //else if (firstName.Length < lastName.Length)
+            //{
+            //    Console.WriteLine("Last must be longer!");
+            //}
+
+            //Console.WriteLine("This is a tab\t.\nThis is a backspace \\.\nAnd this is a bell sound.\a");
+
+            
+            
+            Console.WriteLine("What is your first name?");
+            string firstName = (Console.ReadLine());
+            Console.WriteLine("What is your last name?");
+            string lastName = (Console.ReadLine());
+            Console.WriteLine(firstName + " " + lastName);
+
+        
+
+            
+
+
+
+                
+            
+
+
+
+
+
+
+
+
+
+        
+
+
+
         }
     }
 }
